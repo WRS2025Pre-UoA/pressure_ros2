98f13a3dbd09fbab67cac8ca7f1a5683b1117c0d
rui623 <ruiiwata837@gmail.com>
2024-09-13 15:44:06 +0900

    OK

---
+++ pressure/func_main.py
@@ -0,0 +1,32 @@
+from camera_helper import display_camera_with_guidelines
+from image_processing import process_image
+from needle_angle import calculate_angle
+#pressure_value_from_angle
+
+def m(img):
+    # カメラで補助線を表示して撮影
+    # img=display_camera_with_guidelines()
+
+    # 撮影した画像のパス
+    # image_path = '../data/Origin_data_renamed/test_6.jpg'
+
+    # 画像処理を行い、圧力計の円の中心と針の最長線を取得
+    center_x, center_y, longest_line = process_image(img)
+
+    if longest_line is not None:
+        # 針の角度を計算
+        angle_deg = calculate_angle((center_x, center_y), longest_line)
+
+        # 圧力値を計算（例として最大圧力値を100として計算）
+        #pressure_value = pressure_value_from_angle(angle_deg, max_pressure=100)
+
+        print(f"針の角度: {angle_deg}度")
+        #print(f"圧力計の値: {pressure_value} Pa")
+    else:
+        print("圧力計の針を検出できませんでした。")
+
+def main():
+    m()
+
+if __name__ == "__main__":
+    main()
---
+++ pressure/image_subscriber.py
@@ -0,0 +1,47 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image  # 画像データを受信
+from std_msgs.msg import String  # 結果を返す
+from cv_bridge import CvBridge,CvBridgeError
+import cv2
+import numpy as np
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent))
+import func_main
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(Image, 'image_topic', self.image_callback, 10)
+        self.result_publisher = self.create_publisher(String, 'result_topic', 10)
+        self.bridge = CvBridge()
+
+
+    def image_callback(self, msg):
+        try:
+
+            # ROS 画像メッセージを OpenCV 画像に変換
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+            #画像の処理
+            func_main.m(cv_image)
+
+            # 結果のサイズを計算し、テキストメッセージとしてパブリッシュ
+            result_msg = String()
+            result_msg.data = 'Complete'
+            self.result_publisher.publish(result_msg)
+
+        except CvBridgeError as e:
+            self.get_logger().error(f'Failed to convert image: {e}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
