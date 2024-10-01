4dc67328b9a36e735b1469534490a4ef9431f930
rui623 <ruiiwata837@gmail.com>
2024-10-01 10:29:07 +0900

    change image path part

--- pressure/func_main.py
+++ pressure/func_main.py
@@ -14,83 +14,6 @@ def on_mouse_click(event, x, y, flags, point):
     if event == cv2.EVENT_LBUTTONDOWN:
         point.append((x, y))  # クリックした座標をリストに追加
 
-# def initialize(values):
-#     # 真っ白な背景 (650x400)
-#     img = np.ones((400, 650, 3), dtype=np.uint8) * 255
-
-#     # 長方形の大きさ (150x100)
-#     rect_width = 150
-#     rect_height = 100
-
-#     # 黒い枠線で描く長方形の左上座標
-#     positions = [(50, 75), (250, 75), (450, 75),(50, 215), (250, 215), (450, 215)]
-    
-
-#     # 各長方形に表示するテキスト
-#     # texts = ["1MPa", "0.25MPa", "1.6MPa","2.5MPa","",""]
-#     texts = ["%.2fMPa"%v for v in values]
-
-#     # 描画する枠線の色と太さ
-#     color = (0, 0, 0)  # 黒
-#     thickness = 2
-
-#     # フォント、スケール、色、太さ
-#     font = cv2.FONT_HERSHEY_SIMPLEX
-#     font_scale = 1
-#     font_color = (0, 0, 255)  # 赤
-#     font_thickness = 2
-
-#     point=[]
-
-#     # 各長方形を描画し、中央にテキストを配置
-#     for i, pos in enumerate(positions):
-#         # 長方形の右下座標を計算
-#         top_left = pos
-#         bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
-        
-#         # 黒い枠線の長方形を描画
-#         cv2.rectangle(img, top_left, bottom_right, color, thickness)
-        
-#         # 長方形の中心座標を計算
-#         center_x = top_left[0] + rect_width // 2
-#         center_y = top_left[1] + rect_height // 2
-        
-#         # テキストのサイズを取得して、テキストの位置を調整
-#         text_size = cv2.getTextSize(texts[i], font, font_scale, font_thickness)[0]
-#         text_x = center_x - text_size[0] // 2
-#         text_y = center_y + text_size[1] // 2
-        
-#         # テキストを中央に描画
-#         cv2.putText(img, texts[i], (text_x, text_y), font, font_scale, font_color, font_thickness, cv2.LINE_AA)
-
-#     # 画像を表示
-#     cv2.imshow("Rectangles with Text", img)
-#     cv2.setMouseCallback("Rectangles with Text", on_mouse_click,point)
-#     while len(point) < 1:
-#     # 画像を表示し続ける
-#         key = cv2.waitKey(1)
-#         if key==-1 and cv2.getWindowProperty("Rectangles with Text",cv2.WND_PROP_VISIBLE)<1:
-#             raise ValueError("Closed Window!")
-
-#     cv2.destroyAllWindows()
-#     X = point[0][0]
-#     Y = point[0][1]
-    
-#     if X >= 50 and X < 200: 
-#         if Y >= 75 and Y < 175:
-#             return 1
-#         elif Y >= 215 and Y < 315:
-#             return 4
-#     elif X >= 250 and X < 400:
-#         if Y >= 75 and Y < 175:
-#             return 2
-#         elif Y >= 215 and Y < 315:
-#             return 5
-#     elif X >= 450 and X < 600:
-#         if Y >= 75 and Y < 175:
-#             return 3
-#         elif Y >= 215 and Y < 315:
-#             return 6
 def initialize(values,num):
     # 真っ白な背景 (600x400)
     img = np.ones((400, 600, 3), dtype=np.uint8) * 255
@@ -149,51 +72,64 @@ def initialize(values,num):
         cv2.imshow("Choose Max Value", img)
         cv2.setMouseCallback("Choose Max Value", on_mouse_click, point)
 
-        while len(point) < 1:
+        while True:
             # 画像を表示し続ける
             key = cv2.waitKey(1)
             if key == -1 and cv2.getWindowProperty("Choose Max Value", cv2.WND_PROP_VISIBLE) < 1:
                 raise ValueError("Closed Window!")
+            if len(point)>0:
+                X, Y = point[-1]
 
-        cv2.destroyAllWindows()
+                # ボタンのクリック位置に応じた値を返す
+                for i, pos in enumerate(positions):
+                    top_left = pos
+                    bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
+                    if top_left[0] <= X < bottom_right[0] and top_left[1] <= Y < bottom_right[1]:
+                        if i < len(values):  # 有効な値の範囲内の場合
+                            cv2.destroyAllWindows()
+                            return values[i]  # クリックしたボタンに対応する値を返す
 
-        X, Y = point[0]
+        
+        #         else:
+        #             return None  # 空白ボタンがクリックされた場合
 
-        # ボタンのクリック位置に応じた値を返す
-        for i, pos in enumerate(positions):
-            top_left = pos
-            bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
-            if top_left[0] <= X < bottom_right[0] and top_left[1] <= Y < bottom_right[1]:
-                if i < len(values):  # 有効な値の範囲内の場合
-                    return values[i]  # クリックしたボタンに対応する値を返す
-                else:
-                    return None  # 空白ボタンがクリックされた場合
-
-        return None  # どのボタンにも該当しない場合
+        # return None  # どのボタンにも該当しない場合
     elif num == 2:
         cv2.imshow("Choose Min Value", img)
         cv2.setMouseCallback("Choose Min Value", on_mouse_click, point)
 
-        while len(point) < 1:
+        while True:
             # 画像を表示し続ける
             key = cv2.waitKey(1)
             if key == -1 and cv2.getWindowProperty("Choose Min Value", cv2.WND_PROP_VISIBLE) < 1:
                 raise ValueError("Closed Window!")
 
-        cv2.destroyAllWindows()
+            if len(point)>0:
+                X, Y = point[-1]
 
-        X, Y = point[0]
+                # ボタンのクリック位置に応じた値を返す
+                for i, pos in enumerate(positions):
+                    top_left = pos
+                    bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
+                    if top_left[0] <= X < bottom_right[0] and top_left[1] <= Y < bottom_right[1]:
+                        if i < len(values):  # 有効な値の範囲内の場合
+                            cv2.destroyAllWindows()
+                            return values[i]  # クリックしたボタンに対応する値を返す
 
-        # ボタンのクリック位置に応じた値を返す
-        for i, pos in enumerate(positions):
-            top_left = pos
-            bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
-            if top_left[0] <= X < bottom_right[0] and top_left[1] <= Y < bottom_right[1]:
-                if i < len(values):  # 有効な値の範囲内の場合
-                    print(values[i])
-                    return values[i]  # クリックしたボタンに対応する値を返す
-                else:
-                    return None  # 空白ボタンがクリックされた場合
+        # cv2.destroyAllWindows()
+
+        # X, Y = point[0]
+
+        # # ボタンのクリック位置に応じた値を返す
+        # for i, pos in enumerate(positions):
+        #     top_left = pos
+        #     bottom_right = (top_left[0] + rect_width, top_left[1] + rect_height)
+        #     if top_left[0] <= X < bottom_right[0] and top_left[1] <= Y < bottom_right[1]:
+        #         if i < len(values):  # 有効な値の範囲内の場合
+        #             print(values[i])
+        #             return values[i]  # クリックしたボタンに対応する値を返す
+        #         else:
+        #             return None  # 空白ボタンがクリックされた場合
 
         return None  # どのボタンにも該当しない場合
 
@@ -272,11 +208,11 @@ def main():
     # n = initialize()
     # print(n)
     #image_path = "/home/ros/ros2_ws/src/pressure/pressure/cut_circle.png"
-    # image_path = "/home/ros/ros2_ws/src/pressure/data/cropped_images/LINE_ALBUM_パシャリーズ_240726_2.jpg"#0.25 Not
-    # image_path = "/home/ros/ros2_ws/src/pressure/data/sample/LINE_ALBUM_パシャリーズ_240726_24.jpg"#1
-    # image_path = "/home/ros/ros2_ws/src/pressure/data/sample/LINE_ALBUM_パシャリーズ_240726_25.jpg"#1.6
-    image_path = "/Users/nagasawa/WRS_RosVersion/pressure_ros2/data/sample/LINE_ALBUM_パシャリーズ_240726_3.jpg"#1.6
-    # image_path = "/home/ros/ros2_ws/src/pressure/data/Origin_data/LINE_ALBUM_20240711_240730_54.jpg"#0.25
+    # image_path = "../data/cropped_images/LINE_ALBUM_パシャリーズ_240726_2.jpg"#0.25 Not
+    # image_path = "../data/sample/LINE_ALBUM_パシャリーズ_240726_24.jpg"#1
+    # image_path = "../data/sample/LINE_ALBUM_パシャリーズ_240726_25.jpg"#1.6
+    # image_path = "/Users/nagasawa/WRS_RosVersion/pressure_ros2/data/sample/LINE_ALBUM_パシャリーズ_240726_3.jpg"#1.6
+    image_path = "../data/Origin_data/LINE_ALBUM_20240711_240730_54.jpg"#0.25
     clicks = []
     
     img = cv2.imread(image_path)
